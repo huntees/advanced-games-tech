72e6f32240478d5d81d61e205c7536e693677733
Huntees <alainsiow@gmail.com>
2020-11-13 12:47:15 +0000

    Revised lighting codes between 2d and 3d cam

--- AGT_TEMPLATE/game/src/example_layer.cpp
+++ AGT_TEMPLATE/game/src/example_layer.cpp
@@ -333,7 +333,6 @@ void example_layer::on_render()
 
 	// Set up some of the scene's parameters in the shader
 	std::dynamic_pointer_cast<engine::gl_shader>(textured_lighting_shader)->set_uniform("gEyeWorldPos", m_3d_camera.position());
-	std::dynamic_pointer_cast<engine::gl_shader>(textured_lighting_shader)->set_uniform("lighting_on", true);
 
 	// Position the skybox centred on the player and render it
 	glm::mat4 skybox_tranform(1.0f);
@@ -512,14 +511,19 @@ void example_layer::on_render()
 	m_text_manager->render_text(text_shader, "Orange Text", 10.f, (float)engine::application::window().height() - 25.f, 0.5f, glm::vec4(1.f, 0.5f, 0.f, 1.f));
 
 	//----------------------------------------------------2D Cam-------------------------------------------------------------------------------
+
+	//old code for turning lighting on and off
 	//const auto hud_lighting_shader = engine::renderer::shaders_library()->get("mesh_lighting");
-	std::dynamic_pointer_cast<engine::gl_shader>(textured_lighting_shader)->bind();
-	std::dynamic_pointer_cast<engine::gl_shader>(textured_lighting_shader)->set_uniform("lighting_on", false);
+	//std::dynamic_pointer_cast<engine::gl_shader>(textured_lighting_shader)->bind();
+	//std::dynamic_pointer_cast<engine::gl_shader>(textured_lighting_shader)->set_uniform("lighting_on", false);
 
 	engine::renderer::begin_scene(m_2d_camera, textured_lighting_shader);
 
+	std::dynamic_pointer_cast<engine::gl_shader>(textured_lighting_shader)->set_uniform("lighting_on", false);
+
 	m_intro_screen->on_render(textured_lighting_shader);
 
+	std::dynamic_pointer_cast<engine::gl_shader>(textured_lighting_shader)->set_uniform("lighting_on", true);
 	engine::renderer::end_scene();
 	//----------------------------------------------------2D Cam End--------------------------------------------------------------------------
 }
